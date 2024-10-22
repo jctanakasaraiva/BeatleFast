7bbda47aef9927d8f51571be6549f03e1d880134
jctanakasaraiva <59887192+jctanakasaraiva@users.noreply.github.com>
2024-10-22 19:51:06 -0400

    Add spineball

---
+++ Assets/Scripts/GrowController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowController : MonoBehaviour
+{
+    [SerializeField] private GameObject ObjectToGrow;
+    [SerializeField] private float minScale;
+    [SerializeField] private float maxScale;
+    [SerializeField] private float growSpeed;
+
+    private Vector3 objectScale;
+    private bool triggerActive;
+    
+    
+    private void Start()
+    {
+        GameEvents.Instance.OnGameOver += GameOver;
+    }
+
+    private void Update()
+    {
+        if (triggerActive && objectScale.x < maxScale)
+        {
+            var growth = Time.deltaTime * growSpeed;
+            objectScale += new Vector3(growth, growth);
+            ObjectToGrow.transform.localScale = objectScale;
+        }
+        if (!triggerActive && objectScale.x > minScale)
+        {
+            var growth = Time.deltaTime * growSpeed;
+            objectScale -= new Vector3(growth, growth);
+            ObjectToGrow.transform.localScale = objectScale;
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        triggerActive = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        triggerActive = false;
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.Instance.OnGameOver -= GameOver;
+    }
+
+    private void GameOver(bool gameOver)
+    {
+        Destroy(gameObject);
+    }
+}
